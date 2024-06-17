8201c0ea2f8c74de351d82696ae8cdcecb47608b
Baijnath Kumar <bkum@softwareag.com>
2024-06-17 11:05:53 +0530

    chore: Added projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -35,7 +35,7 @@ const projectsData: Project[] = [
     description: `Imagine being able to travel back in time or to the future. Simple turn the knob
     to the desired date and press "Go". No more worrying about lost keys or
     forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/what-is-edi.jpg',
+    imgSrc: '/static/images/dahadindia.png',
     href: '/blog/dahadindia',
   },
 ]
